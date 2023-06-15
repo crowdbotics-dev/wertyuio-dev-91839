74d1fce6d6e34f80fa404975e097e623ebeec0aa
Crowdbotics <engineering@crowdbotics.com>
2023-06-15 14:25:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wertyuio-dev-91839",
+  url: "https://wertyuio-dev-91839.botics.co",
+  api: "https://wertyuio-dev-91839.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}

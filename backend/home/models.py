d5affa47d115a7cc7f860713054883066410c0b7
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-16 06:10:28 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Mithilesh(models.Model):
+    'Generated Model'
+    studio = models.BigIntegerField()
+    data = models.ForeignKey("home.Mithilesh",on_delete=models.CASCADE,null=True,blank=True,related_name="mithilesh_data",)
